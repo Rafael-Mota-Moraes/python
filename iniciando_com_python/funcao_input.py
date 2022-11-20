12746a196ff1169ea229a45dc7f7939846e833e0
Rafael Moreira <rafaelmota622@gmail.com>
2022-11-20 20:20:20 -0300

    Função input

---
+++ iniciando_com_python/funcao_input.py
@@ -0,0 +1,11 @@
+nome = input('Qual o seu nome? ')
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+soma = numero_1 + numero_2
+
+print(
+    f'{nome} digitou 2 números: {numero_1} e {numero_2}, a soma desses dois números é: {soma}'
+)
+
