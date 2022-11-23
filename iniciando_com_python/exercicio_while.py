2bed955826d98bd2cb473ddcffd6be0c66d84e31
Rafael Moreira <rafaelmota622@gmail.com>
2022-11-23 12:53:02 -0300

    Iterando strings com while

---
+++ iniciando_com_python/exercicio_while.py
@@ -0,0 +1,11 @@
+"""
+Iterando strings com while
+"""
+
+string = 'Rafael'
+tamanho_string = len(string)
+indice = 0
+
+while indice < tamanho_string:
+    print(string[indice], end=' ')
+    indice += 1
