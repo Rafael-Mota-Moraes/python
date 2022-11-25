8f7e3f2457886be731b1d566d4dc931e3bebb5ce
Rafael Moreira <rafaelmota622@gmail.com>
2022-11-25 15:34:08 -0300

    operacao ternária

---
+++ iniciando_com_python/operacao_ternaria.py
@@ -0,0 +1,11 @@
+
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro Valor'
+
+# print(variavel)
+
+digito = 9
+
+novo_digito = digito if digito <= 9 else 0
+novo_digito = 0 if digito > 9 else digito
+print(novo_digito)
