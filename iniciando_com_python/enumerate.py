cd16e068edb696681937eaa0d91f7cd91bfd0f6a
Rafael Moreira <rafaelmota622@gmail.com>
2022-11-25 12:08:11 -0300

    Enumerate

---
+++ iniciando_com_python/enumerate.py
@@ -0,0 +1,10 @@
+lista = ['Rafael', 'Maria', 'Marcelo']
+
+lista_enumerada = list(enumerate(lista))
+
+for indice, nome in lista_enumerada:
+    print(indice, nome)
+
+
+# for item in lista_enumerada:
+#     print(item)
