577fac4f4e58141ffb4b94908cc7eaaeedf244f7
Rafael Moreira <rafaelmota622@gmail.com>
2022-11-26 12:14:53 -0300

    Básico sobre funções

---
+++ python_intermediario/funcoes.py
@@ -0,0 +1,16 @@
+
+# def Print(a, b, c):
+#     print('Minha primeira função!')
+
+# Print()
+
+def imprimir(a, b, c):
+    print(a, b, c)
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+
+def saudacao(nome):
+    print(f'Olá {nome}!')
+
+saudacao('Rafael')
