cff5db7939d6cce1594f3faeef40e53f2c5cc006
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-05 19:31:24 -0300

    Generator expressions

---
+++ python_intermediario/generator_expression.py
@@ -0,0 +1,10 @@
+import sys
+
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable)
+
+lista = [n for n in range(10000)]
+
+generator = (n for n in range(10000))
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
