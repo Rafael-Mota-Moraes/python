dde9653a9098d1aa8350907aef0566058c1fac46
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-06 18:30:21 -0300

    módulos python

---
+++ python_intermediario/modulos.py
@@ -0,0 +1,12 @@
+# import sys
+# from sys import platform
+# sys.exit()
+
+import sys as sistema
+from sys import platform as pt
+
+print(sistema.platform)
+print(pt)
+
+# má pratica
+from sys import *
