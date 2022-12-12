9b70a88538cdcf3e7bd33128e4905a52f91bfab9
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-12 14:10:02 -0300

    Pacotes

---
+++ python_intermediario/packages/__init__.py
@@ -0,0 +1,6 @@
+print(
+    'você importou o package', __name__
+)
+
+def dobra(x):
+    return x * 2
---
+++ python_intermediario/pacotes.py
@@ -0,0 +1,12 @@
+# from sys import path
+# from packages.modulo import soma_do_modulo
+
+# print(__name__)
+# # print(*path, sep='\n')
+# print(soma_do_modulo(1, 2))
+
+# from packages.modulo import soma_do_modulo
+import packages
+
+print(packages.dobra(2))
+print(__name__)
