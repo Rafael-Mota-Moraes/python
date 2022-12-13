f918a68b4e0757d42b5a058ff229ea6bd0d74be8
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-13 13:32:23 -0300

    Funções decoradoras

---
+++ python_intermediario/funcoes_decoradoras.py
@@ -0,0 +1,23 @@
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        for arg in args:
+            is_string(arg)
+
+        resultado = func(*args, **kwargs)
+
+        return resultado
+    return interna
+
+
+def inverte_string(string):
+    return string[::-1]
+
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string')
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+
+invertida = inverte_string_checando_parametro(123)
+print(invertida)
