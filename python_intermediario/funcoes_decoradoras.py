ba963e2aeafaa8b537f66c4fbedf1c61e9e6d1ac
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-13 14:47:56 -0300

    @syntax_sugar

--- python_intermediario/funcoes_decoradoras.py
+++ python_intermediario/funcoes_decoradoras.py
@@ -8,7 +8,7 @@ def criar_funcao(func):
         return resultado
     return interna
 
-
+@criar_funcao
 def inverte_string(string):
     return string[::-1]
 
@@ -17,7 +17,6 @@ def is_string(param):
     if not isinstance(param, str):
         raise TypeError('Param deve ser uma string')
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
 
-invertida = inverte_string_checando_parametro(123)
+invertida = inverte_string('123')
 print(invertida)
