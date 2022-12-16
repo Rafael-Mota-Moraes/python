d28ce5bbd8b0bf43bfdb2ed19c47fb692c6dd5fb
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-16 11:30:40 -0300

    Escopo da classe

---
+++ orientacao_objetos/escopo.py
@@ -0,0 +1,16 @@
+
+class Animal:
+    # nome = 'Leão'
+
+    def __init__(self, nome):
+        self.nome = nome
+        variavel = 'Interna no método init'
+        print(variavel)
+
+    def acao(self):
+        return f'{self.nome} está executando uma ação.'
+
+
+leao = Animal('Leão')
+print(leao.nome)
+print(leao.acao())
