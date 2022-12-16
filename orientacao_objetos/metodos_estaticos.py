3e5e83140257a4a233933bcea985f64337ea8b5d
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-16 16:28:19 -0300

    métodos estáticos

---
+++ orientacao_objetos/metodos_estaticos.py
@@ -0,0 +1,8 @@
+class Classe:
+    @staticmethod
+    def funcao_que_esta_dentro_da_classe():
+        print('Oi')
+
+c1 = Classe()
+c1.funcao_que_esta_dentro_da_classe()
+Classe.funcao_que_esta_dentro_da_classe()
