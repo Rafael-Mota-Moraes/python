60a693a16e3d32d6d9685fba7ee09f77f9ad27bb
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-16 13:37:47 -0300

    Atributos de classe

---
+++ orientacao_objetos/atributos_classe.py
@@ -0,0 +1,15 @@
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade) :
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
+
+p1 = Pessoa('Rafael', 18)
+p2 = Pessoa('Helena', 35)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
