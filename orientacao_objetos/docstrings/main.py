0844cf38f44ce8c06e435930486e5ec0800d8dad
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-21 13:01:27 -0300

    docstrings

---
+++ orientacao_objetos/docstrings/main.py
@@ -0,0 +1,7 @@
+# import uma_linha
+
+# help(uma_linha)
+
+# import varias_linhas
+
+# help(varias_linhas)
---
+++ orientacao_objetos/docstrings/varias_linhas.py
@@ -0,0 +1,16 @@
+"""
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lectus augue,
+ultrices sed sodales sodales, pretium non tortor. Suspendisse potenti. 
+Pellentesque hendrerit venenatis leo non consectetur. Ut et pharetra lacus.
+In quis posuere enim, at maximus ante. Pellentesque rutrum pulvinar vulputate.
+Curabitur egestas viverra condimentum. Aliquam felis nulla, scelerisque vel
+euismod vel, iaculis at metus. Etiam imperdiet arcu vel neque dapibus tempus.
+Phasellus ullamcorper nunc arcu, et pulvinar dolor porta non. Nullam eu 
+scelerisque mi, at interdum risus. Donec magna sapien, congue ut pretium ut,
+mattis a mi. Vestibulum tortor purus, finibus eu elit sit amet, posuere
+consequat est. Etiam felis ante, tincidunt nec posuere in, efficitur a erat.
+Pellentesque magna lectus, sodales non interdum non, pulvinar sed eros. Mauris
+cursus eros non massa iaculis cursus.
+
+"""
