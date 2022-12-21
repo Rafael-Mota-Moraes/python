ab81718b3af5cded61affbcb8b7aa27615e98e6e
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-21 10:30:36 -0300

    Classes decoradoras

---
+++ orientacao_objetos/classes_decoradoras.py
@@ -0,0 +1,20 @@
+class Multiplicar:
+    def __init__(self, func):
+        self.func = func
+        self._multiplicador = 10
+
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado * self._multiplicador
+
+        return interna
+
+
+@Multiplicar(10)
+def soma(x, y):
+    return x * y
+
+
+dois_mais_dois = soma(2, 2)
+print(dois_mais_dois)
