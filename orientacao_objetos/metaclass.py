383a690584a57df88cba6f5eb2b21f2059e0cad6
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-12-21 12:49:00 -0300

    Metaclasses

---
+++ orientacao_objetos/metaclass.py
@@ -0,0 +1,39 @@
+def meu_repr(self):
+    return f'{type(self).__name__}({self.__dict__})'
+
+
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        print('metaclass new')
+        cls = super().__new__(mcs, name, bases, dct)
+        cls.attr = 1234
+        cls.__repr__ = meu_repr
+        
+        if 'falar' not in cls.__dict__:
+            raise NotADirectoryError('Implemente falar')
+
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        instancia = super().__call__(*args, **kwargs)
+        print(instancia.__dict__)
+        return instancia
+
+
+class Pessoa(metaclass=Meta):
+    def __new__(cls, *args, **kwargs):
+        print('Meu new') 
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, nome):
+        print('Meu init')
+        self.nome = nome
+
+    def falar(self):
+        print('Oi')
+
+
+p1 = Pessoa('Rafael')
+print(p1.attr)
+print(p1)
