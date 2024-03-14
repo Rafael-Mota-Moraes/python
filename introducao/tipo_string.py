59f88d471c7b1b4ecfdead8c22e7a44e1a42f1a4
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:26:23 -0300

    Tipo string

---
+++ introducao/tipo_string.py
@@ -0,0 +1,10 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print('Rafael Moreira')
+print("Rafael \"Moreira\"")
+print('Rafael "Moreira"')
