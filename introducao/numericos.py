dfdd6245aa8599a5f402560dd1b18e773807d8b9
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:33:17 -0300

    Tipo int e float

---
+++ introducao/numericos.py
@@ -0,0 +1,20 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# Positivo ou negativo.
+
+print(11)  # int
+print(-11)  # int
+print(0)
+
+# float -> número com ponto flutuante
+# O tipo float representa qualquer número
+
+print(1.1)  # usar ponto como separação
+print(-0.0)
+
+# A função type mostra o tipo que o Python inferiu ao valor
+print(type(1))
+print(type(1.1))
+print(type(0.0))
+print(type("oi"))
