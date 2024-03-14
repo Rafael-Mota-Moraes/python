cc2a10dec046872b3215f8afca6ab89428f0f818
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:18:17 -0300

    A função print

---
+++ introducao/print.py
@@ -0,0 +1,7 @@
+# \r\n -> CRLF
+# \n -> LF
+
+print("Mostra dados na saída padrão do sistema (terminal)")
+print(12, 34, sep="-", end="##")
+print(56, 78, sep="-", end="ACABOU")
+print("\nJá está na outra linha")
