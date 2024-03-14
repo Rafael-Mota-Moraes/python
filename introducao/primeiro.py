52be2bc853fccea61ba700c5a4ef8c1edfd95640
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:11:55 -0300

    Criando meu primeiro módulo Python (*.py)

---
+++ introducao/primeiro.py
@@ -0,0 +1,12 @@
+"""
+DocStrings podem ser usadas como comentários...
+"""
+
+'''
+DocString podem ser também com aspas simples...
+'''
+
+# Isso aqui é um comentário em Python!
+
+print("Olá mundo!")  # Posso escrever aqui um comentário
+print(123)
