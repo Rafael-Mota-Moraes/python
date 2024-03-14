5c60fbf2575d59d135ffbabb7d565211ae5791c7
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:46:05 -0300

    Coerção de tipos

---
+++ introducao/coercao_tipos.py
@@ -0,0 +1,11 @@
+# conversão de tipos, coersão
+# tupe convertion, typecasting, coercion...
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print('a' + 'b')
+print('1', type('1'))
+print(int('1') + 1)
+print(float('1') + 1)  # retorna outro float
+print(float('1.1') + 1.2)  # retorna outro float
