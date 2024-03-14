ce967300e43196fb337f2ffdf5701f44fcdf957a
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-14 10:37:32 -0300

    Tipo bool (boolean)

---
+++ introducao/booleano.py
@@ -0,0 +1,8 @@
+# Tipo de dado bool (boolean)
+# Ao qustionar algo em um programa só existem duas respostas possíveis (verdadeiro ou faso).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que questiona se um valor é igual ao outro
+
+print(10 == 10)
+print(10 != 10)
+print(10 == 11)
