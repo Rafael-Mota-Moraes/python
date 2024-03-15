46ecd09e1ca503d808b760f8b28d59b1352196aa
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-15 15:23:07 -0300

    Recebendo dados do usuário

---
+++ introducao/input.py
@@ -0,0 +1,8 @@
+# nome = input("Qual o seu nome? ")
+# print(f'O seu nome é {nome=}')
+
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+soma = int(n1) + int(n2)
+print("Resultado:", soma)
