5381bc122a384c5b26582f27fe441b9d5e72e290
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-16 12:05:06 -0300

    Operadores in e not in

---
+++ introducao/in_notin.py
@@ -0,0 +1,13 @@
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 5
+#  R a f a e l
+# -6-5-4-3-2-1
+
+nome = "Rafael"
+print(nome[2])
+print(nome[-4])
+
+print('a' in nome)
+print('b' in nome)
+print('b' not in nome)
