d56c2c8bae19567377219af74fedbddb4380251f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-16 10:44:47 -0300

    entendendo o fluxo do interpretador em condicionais

---
+++ introducao/fluxo_ìnterpretador.py
@@ -0,0 +1,28 @@
+condicao = False
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao:
+    print("Esse é o código do if")
+else:
+    print("Esse é o código do else")
+
+if 10 == 10:
+    print("Esse é o outro if")
+
+print("Fora do if")
+
+print()
+
+if condicao1:
+    print("Código para condição 1")
+elif condicao2:
+    print("Código para condição 2")
+elif condicao3:
+    print("Código para condição 3")
+elif condicao4:
+    print("Código para condição 4")
+else:
+    print("Nenhuma condição satisfeita")
