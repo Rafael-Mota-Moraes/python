2478aca9f1302e8b7dcdcc7eeb8e202efb4e5150
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-16 11:31:40 -0300

    operador lógico or

---
+++ introducao/or.py
@@ -0,0 +1,12 @@
+# or - Qualquer condição verdadeira avalia toda a expressão
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input("Senha: ")
+# senha = '123'
+
+# if entrada == 'E' or entrada == 'e' and senha_digitada == senha:
+#     print("Entrar")
+# else:
+#     print("Sair")
+
+print(True and 0 and True)
+print(True or False or 0 or 'abc')
