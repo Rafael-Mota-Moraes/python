f613d1f47e5ee2f9ece5dacddf82d75ef08f5553
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-17 10:27:46 -0300

    Variáveis, constantes e complexidade de código

---
+++ introducao/complexidade_codigo.py
@@ -0,0 +1,25 @@
+"""
+Constante = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+"""
+
+velocidade = 61  # velocidade atual do carro
+local_carro = 100  # local em que o carro está na estrada
+
+RADAR_1 = 60  # velocidade máxima do radar 1
+LOCAL_1 = 100  # local onde o radas 1 está
+RADAR_RANGE = 1  # a distância onde o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (
+    LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+
+if vel_carro_pass_radar_1:
+    print('Velocidade carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print("Carro passou no radar")
+
+if carro_passou_radar_1 and vel_carro_pass_radar_1:
+    print("Carro multado no radar 1")
