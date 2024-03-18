65b4c9cfc13fc1b16f4315fd491ffff06ed48552
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-18 11:30:46 -0300

    Alterado listas

---
+++ introducao/alterando_lista.py
@@ -0,0 +1,13 @@
+"""
+Listas em python
+Tipo list - Mutável
+"""
+
+lista = [10, 20, 30, 40]
+numero = lista[2]
+del lista[2]
+lista.append(50)
+lista.append(60)
+lista.pop()
+print(lista)
+print(numero)
