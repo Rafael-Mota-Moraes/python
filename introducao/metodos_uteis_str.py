7e17d04c74ff7b516a5a19b861bf5bc70dee2840
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-18 13:26:35 -0300

    métodos úteis str

---
+++ introducao/metodos_uteis_str.py
@@ -0,0 +1,10 @@
+frase = 'O rato roeu a roupa do rei de roma'
+lista_palavras = frase.split()
+
+# .strip() - corta espaços
+
+for i, frase in enumerate(lista_palavras):
+    print(lista_palavras[i])
+
+print(lista_palavras)
+nova_frase = join()
