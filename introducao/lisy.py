bb8956f30eb4f03503da16d0974f30fa482e4d7f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-18 11:23:12 -0300

    Tipo list

---
+++ introducao/lisy.py
@@ -0,0 +1,16 @@
+
+"""
+Listas em python
+Tipo list - Mutável
+Suporta vários valores de qalquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+lista = [1, 2, 3, 4, 5]
+print(lista, type(lista))
+print(bool([]), bool(lista))
+
+lista = [123, True, 'Rafael Moreira', 1.2]
+print(lista)
+print(lista[2])
