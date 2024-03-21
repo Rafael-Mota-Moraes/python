b64c526521196527c4b184b3a9575d53da9b4264
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-03-21 11:00:54 -0300

    Closure e funções que retornam outras funções

---
+++ intermediario/closures.py
@@ -0,0 +1,15 @@
+"""
+Closure e função que retornam outras funções.
+"""
+
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+
+
+s1 = criar_saudacao('Bom dia', )
+s2 = criar_saudacao('Boa noite')
+print(s1('Pedro'))
+print(s2('João'))
