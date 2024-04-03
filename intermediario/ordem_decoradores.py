1385a1bdb8dbf99a2a34298cc0e63e13aeb8f421
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-03 10:32:15 -0300

    Ordem de aplicação dos decoradores

---
+++ intermediario/ordem_decoradores.py
@@ -0,0 +1,21 @@
+# Ordem dos decoradores
+
+def parametros_decorador(nome):
+    def decorador(func):
+        print('Decorador:', nome)
+
+        def sua_nova_funcao(*args, **kwargs):
+            res = func(*args, **kwargs)
+            final = f'{res} {nome}'
+            return final
+        return sua_nova_funcao
+    return decorador
+
+
+@parametros_decorador(nome='Primeiro')
+def soma(x, y):
+    return x + y
+
+
+dez_maiz_cinco = soma(10, 5)
+print(dez_maiz_cinco)
