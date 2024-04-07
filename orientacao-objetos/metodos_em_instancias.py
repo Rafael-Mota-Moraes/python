fb16f3e61656e186d7a89556477f4ab251fcb3d4
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-07 14:51:05 -0300

    Métodos em instâncias de classes

---
+++ orientacao-objetos/metodos_em_instancias.py
@@ -0,0 +1,18 @@
+# Métodos em instâncias de classes Python
+
+class Carro:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro('Celta')
+celta.acelerar()
+print(celta.nome)
