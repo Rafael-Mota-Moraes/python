2331107da704aedcb4c5189ec9a31cac01f089aa
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-10 12:51:56 -0300

    Herança múltipla

---
+++ orientacao-objetos/heranca-multipla.py
@@ -0,0 +1,40 @@
+# Herança múltipla
+# Quer dizer que no Python, uma classe pode estender várias outras classes.
+# Herança simples:
+# Animal -> Mamifero -> Humano -> Pessoa -> Cliente
+# Herança múltipla e mixins
+# Log -> FileLog
+# Animal -> Mamifero -> Humano -> Pessoa -> Cliente
+# Cliente(Pessoa, FileLog)
+
+class A:
+    ...
+
+    def quem_sou(self):
+        print('A')
+
+
+class B(A):
+    ...
+
+    def quem_sou(self):
+        print('B')
+
+
+class C(A):
+    ...
+
+    def quem_sou(self):
+        print('C')
+
+
+class D(B, C):
+    ...
+
+    def quem_sou(self):
+        print('D')
+
+
+d = D()
+d.quem_sou()
+print(D.mro())
