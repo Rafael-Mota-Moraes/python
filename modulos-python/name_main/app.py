c791a4740cece27775431ede940bd744087909c1
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-16 15:00:12 -0300

    if __name__ == "__main__"

---
+++ modulos-python/name_main/app.py
@@ -0,0 +1,4 @@
+from modulo import soma
+
+print(soma(10, 30))
+print(soma(20, 20))
---
+++ modulos-python/name_main/modulo.py
@@ -0,0 +1,12 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+def main() -> None:
+    print('Módulo')
+    print(soma(1, 1))
+
+
+print(__name__)
+if __name__ == '__main__':
+    main()
