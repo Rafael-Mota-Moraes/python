e4415e401e2650029f8b97f3a0aed4bb74998a20
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-17 11:15:50 -0300

    datetime.timedelta e dateutil.relativedelta

--- modulos-python/data_hora.py
+++ modulos-python/data_hora.py
@@ -1,6 +1,7 @@
 # Criando datas com módulo datetime
 
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from pytz import timezone
 
 data = datetime.now()
@@ -14,3 +15,19 @@ data = datetime.now(timezone('America/Sao_Paulo'))
 print(data.timestamp())
 data = datetime.strptime(data_str, data_str_fmt)
 print(data)
+
+fmt = '%d/%m/%Y %H:%M:%S'
+data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
+data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+print(data_inicio)
+print(data_fim)
+print(data_fim > data_inicio)
+print(data_fim < data_inicio)
+delta = data_fim - data_inicio
+print(delta.days)
+print(delta.total_seconds())
+delta = timedelta(days=10)
+print(delta)
+print(data_fim + relativedelta(seconds=60))
+delta = relativedelta(data_fim, data_inicio)
+print(delta)
