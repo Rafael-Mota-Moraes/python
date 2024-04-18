b380eafd605e0f88ea4294bb97573a0f5c86f6e1
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-04-18 11:36:35 -0300

    O módulo os para interação com o sistema

---
+++ modulos-python/interacao_sistema.py
@@ -0,0 +1,12 @@
+# Módulo os para interação com o sistema
+# O módulo 'os' forneçe funções para interagir com o sistema operacional.
+# Por exemplo, o módulo os.path contém funções para trabalhar com caminhos de arquivos
+import os
+
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print('a' * 80)
+print(os.system('cls'))
+os.system('echo Olá mundo')
