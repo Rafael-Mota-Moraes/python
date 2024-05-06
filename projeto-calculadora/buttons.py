f231672699f6397f0f6ddd5c444cfc15122058e2
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-05-06 15:37:35 -0300

    configurando a potenciação

--- projeto-calculadora/buttons.py
+++ projeto-calculadora/buttons.py
@@ -3,6 +3,7 @@ from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
 from utils import isNumOrDot, isEmpty, isValidNumber
 from typing import TYPE_CHECKING
+import math
 
 if TYPE_CHECKING:
     from display import Display
@@ -37,8 +38,8 @@ class ButtonsGrid(QGridLayout):
         self.display = display
         self._equation = ''
         self._equationInitialValue = 'Sua conta'
-        self._left = None
-        self._right = None
+        self._left: float | None = None
+        self._right: float | None = None
         self._op = None
 
         self.equation = self._equationInitialValue
@@ -79,7 +80,7 @@ class ButtonsGrid(QGridLayout):
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
 
-        if text in '+-/*':
+        if text in '+-/*^':
             self._connectButtonClicked(
                 button, self._makeSlot(self._operatorClicked, button)
             )
@@ -91,7 +92,7 @@ class ButtonsGrid(QGridLayout):
             )
 
     def _makeSlot(self, func, *args, **kwargs):
-        @ Slot(bool)
+        @Slot(bool)
         def realSlot(_):
             func(*args, **kwargs)
         return realSlot
@@ -137,14 +138,23 @@ class ButtonsGrid(QGridLayout):
 
         self._right = float(displayText)
         self.equation = f'{self._left} {self._op} {self._right}'
-        result = 0.0
+        result = 'error'
+
         try:
-            result = eval(self.equation)
-            print(result)
+            numbersIsNotNone = self._left is not None and self._right is not None
+            if '^' in self.equation and numbersIsNotNone and isinstance(self._left, float):
+                result = math.pow(self._left, self._right)
+            else:
+                result = eval(self.equation)
         except ZeroDivisionError:
             print('ZeroDivisionError')
+        except OverflowError:
+            print('Número muito grande')
 
         self.display.clear()
-        self.info.setText(f'{self.equation} = {result:.2f}')
+        self.info.setText(f'{self.equation} = {result}')
         self._left = result
         self._right = None
+
+        if result == 'error':
+            self._left = None
